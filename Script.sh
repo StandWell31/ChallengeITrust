c0a4c1cf69b134d62049ec21d6bfffac1574f27d
Unknown <xavier.greco@ynov.com>
2017-08-18 11:16:44 +0200

    Script avec début de redirection sorties

---
+++ Script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# On stoppe l'execution du script si on accede a une variable unset
+set -o nounset
+
+# Declaration des variables
+
+# On definit dans quel fichier seront enregistrées les erreurs
+
+if [ -f "monfichier.log"];
+        then rm "monfichier.log";
+fi
+
+exec 4>&2     # sauvegarder stderr
+exec 2>>monfichier.log     #rediriger stderr vers le fichier
+
+echo test
+rm test2
+echo toto
+
+exec 2>&4 # restaurer la valeur de stderr
+exec 4>&- # fermer le FD 4
+
+exit 0
