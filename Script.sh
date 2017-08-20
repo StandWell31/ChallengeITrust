e26be052973001c01262ef9f2845b612c105dd19
Unknown <xavier.greco@ynov.com>
2017-08-20 17:00:10 +0200

    Connection ssh + envoie commande

--- Script.sh
+++ Script.sh
@@ -23,6 +23,14 @@ grep -vi windows ips.csv | tr -d '"' > ips_sans_windows.csv
 #rm ips.csv
 cut -d',' -f1 ips_sans_windows.csv > ips_only.csv
 
+USERNAME=xavier
+HOSTS="192.168.21.130"
+SCRIPT="touch test"
+for HOSTNAME in ${HOSTS} ; do
+        sshpass -p "root" ssh -o StrictHostKeyChecking=no  -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+done
+
+rm ips_sans_windows.csv ips_only.csv
 
 
 #################################################################
